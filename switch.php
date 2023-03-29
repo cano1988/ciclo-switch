109a8d6b8ef604527fcb6c07666ab11bd41546cc
andres1988 <a.canorave@gmail.com>
2023-03-29 11:45:44 -0500

    ciclo finalizado

---
+++ switch.php
@@ -0,0 +1,21 @@
+<?php
+
+$i = 0;
+switch ($i) {
+  case 0:
+      echo "i es igual a 0";
+      break;
+  case 1:
+      echo "i es igual a 1";
+      break;
+  case 2:
+      echo "i es igual a 2";
+      break;
+  default:
+      echo "i no es igual al valor asignado";   
+}
+?>
+
+
+
+
